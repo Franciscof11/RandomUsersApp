44843297d3fc6117bb11ae956c4ecceec6d5eb1b
Francisco Gabriel <franciscogabrielf11@gmail.com>
2022-10-29 17:28:20 -0300

    finalizing project

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,41 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:usuarios/connectivity/connectivity_store.dart';
+import 'package:usuarios/users/user_repository.dart';
+import 'package:usuarios/users/users_api.dart';
+import 'package:usuarios/users/users_store.dart';
 
-import 'app/screens/HomePage.dart';
+import 'home/home_page.dart';
 
-void main() {
-  runApp(const RandomUsersApp());
+GetIt getIt = GetIt.I;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory directory = await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+  Hive.openBox('users');
+  getIt.registerSingleton(Client());
+  getIt.registerFactory(() => UsersAPIList(getIt()));
+  getIt.registerSingleton<ConnectivityStore>(ConnectivityStore());
+  getIt.registerSingleton<UsersRepository>(UsersRepository(getIt()));
+  getIt.registerSingleton<UsersStore>(UsersStore(getIt()));
+  runApp(const MyApp());
 }
 
-class RandomUsersApp extends StatelessWidget {
-  const RandomUsersApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Random_Users_App',
+      title: 'Flutter Demo',
       theme: ThemeData(
+        useMaterial3: true,
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
